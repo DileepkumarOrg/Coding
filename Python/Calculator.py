deae7ef46140a4008cdcf30d74bfa872c92b6be5
Dileepkumar74 <dileepkumarrouthu1441@gmail.com>
2024-08-15 09:58:43 +0530

    Dileep

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,2 +1,11 @@
-import area as a
-print("Enter your choice : \n1.Addition         \n2.Substraction\3")
+def addition(num1,num2):
+    return num1+num2
+def subtraction(num1,num2):
+    return num1-num2
+def multiplication(num1,num2):
+    return num1*num2
+def division(num1,num2):
+    return num1/num2
+print("Select the operation: ")
+print("1)Addition ")
+c=int(input())
---
+++ Python/even_odd_digit_sum_differ.py
@@ -0,0 +1,9 @@
+num=input(" ")
+even=0
+odd=0
+for i in range(len(num)):
+    if i%2==0:
+        even+=int(num[i])
+    else:
+        odd+=int(num[i])
+print(abs(even-odd))
